0f04b3a2dc7a89b44e324948d910f21bdfa1c5bd
Administrator <blackof2012@163.com>
2016-08-18 10:18:13 +0800

    MQTT好了

---
+++ platforms/android/src/com/tky/mqtt/paho/callback/MqttMessageCallback.java
@@ -0,0 +1,75 @@
+package com.tky.mqtt.paho.callback;
+
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+import com.ionicframework.im366077.MainActivity;
+import com.tky.mqtt.paho.ConnectionType;
+import com.tky.mqtt.paho.MessageOper;
+import com.tky.mqtt.paho.MqttNotification;
+import com.tky.mqtt.paho.ReceiverParams;
+import com.tky.mqtt.paho.UIUtils;
+import com.tky.mqtt.paho.bean.MessageBean;
+import com.tky.mqtt.paho.sync.MqttConnection;
+import com.tky.mqtt.paho.utils.GsonUtils;
+import com.tky.mqtt.paho.utils.NetUtils;
+
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+
+public class MqttMessageCallback implements MqttCallback {
+
+    private Context context;
+    private final MqttConnection mqttAsyncClient;
+    /**
+     * 重连次数
+     */
+    private static int count = 0;
+
+    public MqttMessageCallback(Context context, MqttConnection mqttAsyncClient) {
+        this.context = context;
+        this.mqttAsyncClient = mqttAsyncClient;
+    }
+
+    @Override
+    public void connectionLost(Throwable arg0) {
+        Log.d("reconnect", "断掉了，哥们~~~" + (mqttAsyncClient == null ? "nullllll" : "notnulll"));
+        if (NetUtils.isConnect(context) && mqttAsyncClient.getConnectionType() != ConnectionType.MODE_CONNECTION_DOWN_MANUAL) {
+            try {
+                mqttAsyncClient.reconnect();
+            } catch (MqttException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void deliveryComplete(IMqttDeliveryToken arg0) {
+    }
+
+    @Override
+    public void messageArrived(final String topic, final MqttMessage msg) throws Exception {
+        Log.d("messageArrived", new String(msg.getPayload()));
+        final MessageBean map = MessageOper.unpack(msg.getPayload());
+		final String msgTopic = (String) map.getSessionid();
+		final String msgContent = (String) map.getMessage();
+		UIUtils.runInMainThread(new Runnable() {
+
+            @Override
+            public void run() {
+                MqttNotification.showNotify(msgTopic, msgContent, new Intent(context, MainActivity.class));
+                Intent intent = new Intent();
+                intent.setAction(ReceiverParams.MESSAGEARRIVED);
+                intent.putExtra("topic", topic);
+                String json = GsonUtils.toJson(map, MessageBean.class);
+                intent.putExtra("content", json);
+                intent.putExtra("qos", msg.getQos());
+                msg.clearPayload();
+                context.sendBroadcast(intent);
+            }
+        });
+    }
+}
