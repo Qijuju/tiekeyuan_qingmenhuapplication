1bac51c1c9c126ddcedc80d9da5b441e8ae2eb79
Administrator <blackof2012@163.com>
2016-08-29 10:20:36 +0800

    更新最新代码

--- platforms/android/src/com/tky/mqtt/paho/BaseApplication.java
+++ platforms/android/src/com/tky/mqtt/paho/BaseApplication.java
@@ -11,70 +11,76 @@ import com.tky.mqtt.dao.DaoSession;
 import java.io.File;
 
 public class BaseApplication extends Application {
-	private static Context context;
-	private static int mainThreadId;
-	private static Thread mainThread;
-	private static Handler handler;
-	private static BaseApplication mInstance;
-	private static DaoMaster daoMaster;
-	private static DaoSession daoSession;
-	public static final String DB_NAME=Environment.getExternalStorageDirectory().getPath()
-	+ File.separator+"TKY"+File.separator+"KKK";//测试版本数据库路径
-//public static final String DB_NAME= "KKK";//正式发布版本数据库路径
-	@Override
-	public void onCreate() {
-		super.onCreate();
-		//Context
-		context = getApplicationContext();
-		//mainThreadId
-		mainThreadId = android.os.Process.myTid();
-		//Thread-->object
-		mainThread = Thread.currentThread();
-		//Handler
-		handler = new Handler();
-		if(mInstance == null){
-			mInstance = this;
-		}
-	}
-	public static Context getContext() {
-		return context;
-	}
-	public static int getMainThreadId() {
-		return mainThreadId;
-	}
-	public static Thread getMainThread() {
-		return mainThread;
-	}
-	public static Handler getHandler() {
-		return handler;
-	}
-	/**
-	 * 取得DaoMaster
-	 *
-	 * @param context
-	 * @return
-	 */
-	public static DaoMaster getDaoMaster(Context context) {
-		if (daoMaster == null) {
-			DaoMaster.OpenHelper helper = new DaoMaster.DevOpenHelper(context, DB_NAME, null);
-			daoMaster = new DaoMaster(helper.getWritableDatabase());
-		}
-		return daoMaster;
-	}
+  private static Context context;
+  private static int mainThreadId;
+  private static Thread mainThread;
+  private static Handler handler;
+  private static BaseApplication mInstance;
+  private static DaoMaster daoMaster;
+  private static DaoSession daoSession;
+  /*public static final String DB_NAME = Environment.getExternalStorageDirectory().getPath()
+    + File.separator + "TKY" + File.separator + "KKK";//测试版本数据库路径
+*/
+  public static final String DB_NAME= "KKK";//正式发布版本数据库路径
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    //Context
+    context = getApplicationContext();
+    //mainThreadId
+    mainThreadId = android.os.Process.myTid();
+    //Thread-->object
+    mainThread = Thread.currentThread();
+    //Handler
+    handler = new Handler();
+    if (mInstance == null) {
+      mInstance = this;
+    }
+  }
 
-	/**
-	 * 取得DaoSession
-	 *
-	 * @param context
-	 * @return
-	 */
-	public static DaoSession getDaoSession(Context context) {
-		if (daoSession == null) {
-			if (daoMaster == null) {
-				daoMaster = getDaoMaster(context);
-			}
-			daoSession = daoMaster.newSession();
-		}
-		return daoSession;
-	}
+  public static Context getContext() {
+    return context;
+  }
+
+  public static int getMainThreadId() {
+    return mainThreadId;
+  }
+
+  public static Thread getMainThread() {
+    return mainThread;
+  }
+
+  public static Handler getHandler() {
+    return handler;
+  }
+
+  /**
+   * 取得DaoMaster
+   *
+   * @param context
+   * @return
+   */
+  public static DaoMaster getDaoMaster(Context context) {
+    if (daoMaster == null) {
+      DaoMaster.OpenHelper helper = new DaoMaster.DevOpenHelper(context, DB_NAME, null);
+      daoMaster = new DaoMaster(helper.getWritableDatabase());
+    }
+    return daoMaster;
+  }
+
+  /**
+   * 取得DaoSession
+   *
+   * @param context
+   * @return
+   */
+  public static DaoSession getDaoSession(Context context) {
+    if (daoSession == null) {
+      if (daoMaster == null) {
+        daoMaster = getDaoMaster(context);
+      }
+      daoSession = daoMaster.newSession();
+    }
+    return daoSession;
+  }
 }
