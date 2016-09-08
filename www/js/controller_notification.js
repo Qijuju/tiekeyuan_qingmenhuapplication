f1b4a3e82a1d6edda63a07d83d8ca2de6a5d19ac
Administrator <xiao58421xiao@163.com>
2016-09-08 14:53:11 +0800

    11

---
+++ www/js/controller_notification.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Administrator on 2016/9/8.
+ */
+angular.module('notification.controllers', [])
+
+  .controller('notificationCtrl', function ($scope, $stateParams, Chats) {
+
+     alert('wolaileni')
+  });
