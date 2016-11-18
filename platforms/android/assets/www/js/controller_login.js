dd8d9399a414b0e6dff685d839c477f7aaba1f29
Administrator <xiao58421xiao@163.com>
2016-11-18 11:18:02 +0800

    123321

--- platforms/android/assets/www/js/controller_login.js
+++ platforms/android/assets/www/js/controller_login.js
@@ -227,7 +227,7 @@ angular.module('login.controllers', [])
   })
 
   .controller('newsPageCtrl', function ($scope, $state, $ionicPopup, $ionicLoading, $cordovaFileOpener2, $http, $mqtt, $cordovaPreferences, $api, $rootScope,$ToastUtils,$timeout,$interval) {
-
+   
     document.getElementById("imgaaa").style.height=(window.screen.height)+'px';
     var passworda="";
     var loginpageaa=""
