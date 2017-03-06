46504b8cfdefaeced6cdbe44552e17f7bd377832
slsxiaoq <blackof2012@163.com>
2017-03-06 16:12:59 +0800

    更新liuxiaowei代码

--- platforms/android/assets/www/js/controller_message.js
+++ platforms/android/assets/www/js/controller_message.js
@@ -4222,7 +4222,7 @@ angular.module('message.controllers', [])
     var backButtonPressedOnceToExit=false;
     $ionicPlatform.registerBackButtonAction(function (e) {
       if($location.path()==('/tab/message/'+$scope.ID+'/'+$scope.SESSIONID+'/'+$scope.GROUP)||$location.path()=='/tab/notification'||$location.path()=='/tab/contacts'||$location.path()=='/tab/account'||$location.path()=='/login'||$location.path()=='/tab/webpage'){
-        
+
         if (backButtonPressedOnceToExit) {
           $mqtt.setExitStartedStatus();
           ionic.Platform.exitApp();
