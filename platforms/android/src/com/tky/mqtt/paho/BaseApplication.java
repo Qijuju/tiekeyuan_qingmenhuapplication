5c23fdf5b36fc30fa8123a665967dbe457a72b48
Administrator <blackof2012@163.com>
2017-03-29 16:25:38 +0800

    合并后最新代码提交

--- platforms/android/src/com/tky/mqtt/paho/BaseApplication.java
+++ platforms/android/src/com/tky/mqtt/paho/BaseApplication.java
@@ -36,10 +36,10 @@ public class BaseApplication extends DefaultApplicationLike {
   private static BaseApplication mInstance;
   private static DaoMaster daoMaster;
   private static DaoSession daoSession;
-  public static final String DB_NAME= Environment.getExternalStorageDirectory().getPath()
-    + File.separator+"TKY"+ File.separator+"KKK";//测试版本数据库路径
+//  public static final String DB_NAME= Environment.getExternalStorageDirectory().getPath()
+//    + File.separator+"TKY"+ File.separator+"KKK";//测试版本数据库路径
   private boolean isInBackground;
-  //public static final String DB_NAME = "KKK";//正式发布版本数据库路径
+  public static final String DB_NAME = "KKK";//正式发布版本数据库路径
 
 
 
