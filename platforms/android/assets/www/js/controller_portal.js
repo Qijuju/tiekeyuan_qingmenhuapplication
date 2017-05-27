8cea3e492d0f823c709896fad62833f340b75c4a
Administrator <13641134362@163.com>
2017-05-27 16:03:08 +0800

    轻门户改造

--- platforms/android/assets/www/js/controller_portal.js
+++ platforms/android/assets/www/js/controller_portal.js
@@ -2,13 +2,13 @@
  * Created by Administrator on 2017/3/24.
  */
 angular.module('portal.controllers', [])
-  .controller('portalCtrl', function ($scope,$ToastUtils, $mqtt, $state, $ionicSlideBoxDelegate, $ionicLoading, NetData, $greendao,FinshedApp,$rootScope) {
+  .controller('portalCtrl', function ($scope, $ToastUtils, $mqtt, $state, $ionicSlideBoxDelegate, $ionicLoading, NetData, $greendao, FinshedApp, $rootScope, $http) {
 
     $scope.$on('netstatus.update', function (event) {
       $scope.$apply(function () {
         //alert("哈哈哈哈哈啊哈哈哈哈");
         //   alert("关网时走不走"+$rootScope.netStatus);
-        $rootScope.isConnect=$rootScope.netStatus;
+        $rootScope.isConnect = $rootScope.netStatus;
         // alert("切换网络时"+$scope.isConnect);
       })
     });
@@ -22,80 +22,76 @@ angular.module('portal.controllers', [])
       showDelay: 0
     });
 
-    $scope.$on('msgs.update', function (event) {
-      $scope.$apply(function () {
-        // alert("进来单聊界面吗？");
-        $greendao.queryByConditions('ChatListService',function (data) {
-          $chatarr.setData(data);
-          $scope.items=data;
-          // alert("数组的长度"+data.length);
-        },function (err) {
-
-        });
-        $timeout(function () {
-          viewScroll.scrollBottom();
-        }, 100);
-      })
-    });
-
-      var userID;
+    var userID;
+    $scope.$on('$ionicView.enter', function () {
       $mqtt.getUserInfo(function (succ) {
         userID = succ.userID;
         // userID=109975;
-        //rxy 第一次http得到人员所在的部门
-        NetData.getCompanyName(userID);
-        //rxy 第二次http得到人员可以查看的应用(点亮图标）
-        NetData.getAppMenu(userID);
+        // userID = 770;
+
+        //获取人员所在部门，点亮图标
+        NetData.getInfo(userID);
+
 
       }, function (err) {
         $ionicLoading.hide();
         $ToastUtils.showToast("网络错误");
       });
+    })
+
+    $scope.$on('succeed.update', function (event) {
+
+      $scope.companyName = NetData.getName();
+      //点亮图标
+      var lightApps = NetData.getLightApps();
+      for (var i = 0; i < lightApps.length; i++) {
+        document.getElementById("" + lightApps[i].appId).src = lightApps[i].appIcon;
+      }
+      $ionicLoading.hide();
+    });
 
 
-      $scope.$on('companyName.update', function (event) {
+    $scope.$on('error.update', function () {
+      $ionicLoading.hide();
+      $ToastUtils.showToast("网络错误");
+    });
 
-        $scope.companyName = NetData.getName();
 
-      });
+    //rxy 跳转页面详情
+    $scope.jumpUrl = function (appId) {
+      $http({
+        method: 'get',
+        timeout: 5000,
+        url: "http://61.237.239.152:8080/apiman-gateway/jishitong/getsession/1.0?apikey=b8d7adfb-7f2c-47fb-bac3-eaaa1bdd9d16&id=" + userID
+      }).success(function (data, status) {
 
-      $scope.$on('error.update', function () {
-        $ionicLoading.hide();
-        $ToastUtils.showToast("网络错误");
-
-      });
-      $scope.$on('appMenu.update', function () {
-        $ionicLoading.hide();
-        var lightApps = NetData.getLightApps();
-        console.log(NetData.getLightApps());
-        // alert(lightApps.length);
-        for (var i = 0; i < lightApps.length; i++) {
-          document.getElementById("" + lightApps[i].appId).src = lightApps[i].appIcon;
+        if (data.sessionid == null && typeof(data.sessionid) == "undefined") {
+          $ToastUtils.showToast("获取用户权限失败!");
+          return;
         }
-      });
-      //rxy 跳转页面详情
-      $scope.jumpUrl = function (appId) {
-        //userId  用户的id后期动态获取
+
         if (NetData.get(appId) != null) {
           $state.go("cxtx", {
             "appId": appId,
             "userId": userID,
-            "appUrl": FinshedApp.get(appId).appUrl
+            // "appUrl": "http://61.237.239.105:18190/h5app/DMSAppPage/UI/index.aspx?token=" + data.sessionid
+            "appUrl": NetData.get(appId).appUrl + data.sessionid
           });
-        } else {
-          // alert("该应用尚未上线");
         }
-      };
+
+      }).error(function (data, status) {
+        $ToastUtils.showToast("获取用户权限失败!")
+      });
+    };
+
   })
-
-
 
 
   //rxy 详情的控制器
   .controller('OhterCtrl', function ($scope, $state, $stateParams, $sce, FinshedApp) {
     var str;
     // $scope.userid=$stateParams.userId;    + "?userId=" + $stateParams.userId;
-    $scope.appurl = $sce.trustAsResourceUrl($stateParams.appUrl+"?userId="+$stateParams.userId);
+    $scope.appurl = $sce.trustAsResourceUrl($stateParams.appUrl);
     // $scope.appurl = $sce.trustAsResourceUrl($stateParams.appUrl+"?userId=499");
     $scope.appName = FinshedApp.get($stateParams.appId).appName;
     // str=$stateParams.appUrl+"?userId="+$stateParams.userId;
@@ -103,8 +99,12 @@ angular.module('portal.controllers', [])
 
 
     $scope.goBack = function () {
-      window.history.back();
-      // $state.go("tab.dash");
+      // window.history.back();
+      $state.go("tab.portal");
     }
 
   });
+
+
+
+
