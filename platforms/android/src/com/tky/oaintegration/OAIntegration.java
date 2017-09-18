3a4d898dea587ed830491c2e0888c2f881e00295
qjj <1534389486@qq.com>
2017-09-18 17:25:20 +0800

    修改门户模块，进入门户默认显示新闻通知列表。

--- platforms/android/src/com/tky/oaintegration/OAIntegration.java
+++ platforms/android/src/com/tky/oaintegration/OAIntegration.java
@@ -214,7 +214,7 @@ public class OAIntegration extends CordovaPlugin {
             map.put("type",type);
             map.put("appId", appId);
             map.put("platform", platform);
-            String filePath = Environment.getExternalStorageDirectory().getAbsolutePath() + File.separator + "NewEMCAPP";
+            String filePath = Environment.getExternalStorageDirectory().getAbsolutePath() + File.separator + "LPREMPLAT";
             OkHttpUtils.get()
                     .url(Constants.commonfileurl + "/DownloadFile")
                     .params(map)
@@ -251,7 +251,8 @@ public class OAIntegration extends CordovaPlugin {
                         @Override
                         public void inProgress(float progress, long total, int id) {
                             super.inProgress(progress, total, id);
-                            progress = (progress * (-1.0f)) / filesize * 100;
+                            progress = progress *  100;
+//                            progress = (progress * (-1.0f)) / filesize * 100;
                             final float finalProgress = progress;
                             UIUtils.runInMainThread(new Runnable() {
                                 @Override
